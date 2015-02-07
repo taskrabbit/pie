d56418cea67314397d219bbfcc284f069d723aab
Mike Nelson <mike@mnelson.io>
2015-02-06 21:57:17 -0500

    larger templatse

--- spec/performanceSpec.js
+++ spec/performanceSpec.js
@@ -4,6 +4,24 @@ describe("pie performance", function() {
 
   describe("templating", function() {
 
+    var originalTimeout;
+
+    beforeEach(function() {
+      originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+      jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
+    });
+
+    afterEach(function() {
+      jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    });
+
+    var expand = function(str) {
+      for(var i = 0; i < 10; i++) {
+        str += str;
+      }
+      return str;
+    };
+
     var resig = function (str) {
       var strFunc =
       "var p=[];" +
@@ -26,6 +44,12 @@ describe("pie performance", function() {
     var ejsSource = "<% for(var i = 0; i < 2; i++) { %><span><%= foo + i %></span><% } %>";
 
     it("should perform better than EVERYONE", function(done) {
+
+      if(!app.navigator.get('query.bm')) {
+        expect(1).toEqual(1);
+        return done();
+      }
+
       app.resources.load(
         'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js',
         'http://cdn.kendostatic.com/2011.2.804/js/kendo.all.min.js',
@@ -33,12 +57,14 @@ describe("pie performance", function() {
         '/vendor/ejs.js', function(){
 
         var suite = new Benchmark.Suite();
-        var resigTmpl = resig(source);
-        var pieTmpl = pie.string.template(source);
-        var kendoTmpl = kendo.template(kendoSource);
-        var _Tmpl = _.template(_Source);
-        var ejsTmpl = new EJS({text: ejsSource});
+        var resigTmpl = resig(expand(source));
+        var pieTmpl = pie.string.template(expand(source));
+        var kendoTmpl = kendo.template(expand(kendoSource));
+        var _Tmpl = _.template(expand(_Source));
+
+        var ejsTmpl = new EJS({text: expand(ejsSource)});
         ejsTmpl = ejsTmpl.render.bind(ejsTmpl);
+
         var d = {foo: 4};
 
         var cases = {
@@ -49,8 +75,10 @@ describe("pie performance", function() {
           ejs: function() { return ejsTmpl(d); }
         };
 
+        var expectedOutput = expand('<span>4</span><span>5</span>');
+
         pie.object.forEach(cases, function(k,v) {
-          expect(v()).toEqual('<span>4</span><span>5</span>');
+          expect(v()).toEqual(expectedOutput);
           suite.add(k, v);
         });
 
@@ -61,11 +89,11 @@ describe("pie performance", function() {
         suite.on('complete', function() {
           var winner = this.filter('fastest').pluck('name');
           expect(~winner.indexOf('pie')).toBeTruthy();
-          console.log('Fastest is ' + winner);
+          console.log('Winner: ' + winner);
           done();
         });
 
-        suite.run({minSamples: 200});
+        suite.run();
       });
 
     });
