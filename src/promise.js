26ca5f7eaa0f0dd23ec67f65fce24d25a34b3bdc
Mike Nelson <mike@mnelson.io>
2015-06-15 10:07:41 -0400

    promise/a+ compliant (i think)

--- src/promise.js
+++ src/promise.js
@@ -2,54 +2,130 @@ pie.promise = pie.base.extend({
 
   __pieRole: 'promise',
 
-  UNFULFILLED: 'UNFULFILLED',
-  REJECTED: 'REJECTED',
-  FULFILLED: 'FULFILLED',
-
   init: function(resolver) {
-    this.resolver = resolver;
 
-    this.fulfills = [];
-    this.rejects = [];
+    this.resolves = [];
+    this.rejects  = [];
 
-    this.fulfillArgs  = undefined;
-    this.rejectArgs   = undefined;
+    this.result   = undefined;
+    this.state    = 'UNFULFILLED';
 
-    this.state = this.UNFULFILLED;
-
-    try {
-      this.resolver(this.fulfillProxy.bind(this), this.rejectProxy.bind(this));
-    } catch(ex) {
-      this.rejectProxyProxy(ex);
+    if(resolver) {
+      setTimeout(function(){
+        try {
+          resolver(this.resolve.bind(this), this.reject.bind(this));
+        } catch(ex) {
+          this.reject(ex);
+        }
+      }.bind(this), 0);
     }
   },
 
-  then: function(fulfill, reject) {
+  then: function(onResolve, onReject) {
+    var child = this.__class.create();
 
-    if(fulfill) this.fulfills.push(fulfill);
-    if(reject) this.rejects.push(reject);
+    this.resolves.push([onResolve, child]);
+    this.rejects.push([onReject, child]);
 
-    if(this.state === this.REJECTED) {
-      this.rejectProxy.apply(this, this.rejectArgs);
-    }
-    else if(this.state === this.FULFILLED) {
-      this.fulfillProxy.apply(this, this.fulfillArgs);
+    if(this.state !== 'UNFULFILLED') {
+      setTimeout(this._flush.bind(this), 0);
     }
 
-    return this.__class.create(pie.fn.noop);
+    return child;
   },
 
   catch: function(reject) {
-    this.then(undefined, reject);
+    return this.then(undefined, reject);
   },
 
-  rejectProxy: function() {
-    var args = this.rejectArgs = arguments;
-    // if(this.reject) this.reject.apply(null, arguments);
+  reject: function(value) {
+    this._transition('FAILED', value);
   },
 
-  fulfillProxy: function() {
-    // if(this.fulfill) this.fulfill.apply(null, arguments);
+  resolve: function(value) {
+    this._transition('FULFILLED', value);
+  },
+
+  _transition: function(state, value) {
+    if(this.state === 'UNFULFILLED') {
+      this.state = state;
+      this.result = value;
+      this._flush();
+    }
+  },
+
+  _flush: function() {
+    var list = this.state === 'FULFILLED' ? this.resolves : this.rejects;
+    var tuple, callback, promise, result;
+
+    while(list.length) {
+      tuple = list.pop();
+      callback = tuple[0];
+      promise = tuple[1];
+
+      if(pie.object.isFunction(callback)) {
+        try {
+          result = callback(this.result);
+        } catch(e) {
+          promise.reject(e);
+          continue;
+        }
+
+        if(result === promise) {
+          promise.reject(new TypeError("The result of a promise's callback cannot be the promise. [2.3.1]"));
+          continue;
+        }
+
+        if(pie.object.isPromise(result)) {
+          result.then(promise.resolve.bind(promise), promise.reject.bind(promise));
+        } else {
+          promise.resolve(result);
+        }
+
+      } else if(this.state === 'FULFILLED') {
+        promise.resolve(this.result);
+      } else {
+        promise.reject(this.result);
+      }
+    }
+  }
+});
+
+pie.promise.all = function(iteratable) {
+  var instance = new pie.promise(),
+  promises = [],
+  values = [],
+  cnt = 0,
+  p;
+
+  for(var k in iteratable) {
+    if(iteratable.hasOwnProperty(k)) {
+      p = iteratable[k];
+      if(!pie.object.isPromise(p)) p = pie.promise.resolve(p);
+      promises.push(p);
+    }
   }
 
-});
+  promises.forEach(function(p, i) {
+    p.then(function(val) {
+      values[i] = val;
+      if(cnt === values.length) instance.resolve(values);
+    }, instance.reject.bind(instance));
+  });
+
+  return instance;
+};
+
+pie.promise.resolve = function(val) {
+  var p = pie.promise.create();
+  p.state = 'FULFILLED';
+  p.result = val;
+  return p;
+};
+
+pie.promise.reject = function(val) {
+  var p = pie.promise.create();
+  p.state = 'FAILED';
+  p.result = val;
+  return p;
+};
