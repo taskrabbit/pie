fb0b4fefb6f4a1030d16a1792dbf2f4b32c8eae5
Mike Nelson <mike@mnelson.io>
2015-09-02 10:38:43 -0400

    remove dom-defined data bindings

--- src/mixins/bindings.js
+++ src/mixins/bindings.js
@@ -7,7 +7,6 @@ pie.mixins.bindings = {
   init: function() {
     this._bindings = [];
     if(this._super) this._super.apply(this, arguments);
-    this.options.bindingAttribute = this.options.bindingAttribute || 'data-bind';
   },
 
   // If we have an emitter, tap into the render:after event and initialize the dom
@@ -34,50 +33,7 @@ pie.mixins.bindings = {
     }
   },
 
-  setupDomBindings: function() {
-    if(!this.options.bindingAttribute) return;
-
-    this.removeUnattachedBindings();
-
-    var nodes = this.qsa('[' + this.options.bindingAttribute + ']'), stringOpts, node, opts;
-
-    var binder = function(str) {
-      if(str.indexOf('=') === -1) opts = {attr: str};
-      else opts = this.parseStringBinding(str);
-      opts.sel = node;
-      this.bind(opts);
-    }.bind(this);
-
-    for(var i = 0; i < nodes.length; i++) {
-      node = nodes[i];
-      stringOpts = node.getAttribute(this.options.bindingAttribute);
-      stringOpts.split(';').forEach(binder);
-    }
-  },
-
-  removeUnattachedBindings: function() {
-    var compactNeeded = false, b;
-    for(var i = 0; i < this._bindings.length; i++) {
-      b = this._bindings[i];
-      if(pie.object.isNode(b.sel) && !document.contains(b.sel)) {
-        this._bindings[i] = undefined;
-        b.teardown();
-        compactNeeded = true;
-      }
-    }
-
-    if(compactNeeded) this._bindings = pie.array.compact(this._bindings);
-  },
-
-  parseStringBinding: function(inputString) {
-    var opts = pie.object.expand(pie.string.deserialize(inputString));
-    return opts;
-  },
-
   initBindings: function() {
-    // look for dom-defined bindings and add them to our _bindings list.
-    this.setupDomBindings();
-
     // Iterate each binding and propagate the model value to the dom.
     pie.array.each(this._bindings, 'toView', true);
   },
